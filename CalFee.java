72f2197df11164daf5bc31c415c2b3ff558003de
태영준 <ytae1999@naver.com>
2020-06-10 18:33:26 +0900

    Cal 메소드 일부 작성

--- CalFee.java
+++ CalFee.java
@@ -7,7 +7,7 @@
  */
 public class CalFee
 {
-    private int cost;
+    private double cost;
     private boolean old;
     /**
      * 할인대상을 확인해서 할인된 요금을 계산해주는 메소드
@@ -19,9 +19,17 @@ public class CalFee
      *               구작(대여시작일로부터 6개월이상 경과된 작품)은 반액
      *               단, 2가지 이상의 할인적용조건이 중복되는 경우, 할인율이 높은 조건을 우선함.
      */
-    public int Cal(int age, boolean old)
+    public double Cal(int age, boolean old)
     {
-        
+        if(age >= 1 && age <= 18){
+            this.cost = cost - (cost*0.1);
+        }
+        if(age >= 65){
+            this.cost = cost - (cost*0.2);
+        }
+        if(old == true){
+            this.cost = cost - (cost*0.5);
+        }
         return cost;
     }
 }
